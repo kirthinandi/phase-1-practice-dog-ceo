efa43c266313c08b8c657ce33088701f19732adb
Kirthi Nandi <kirthi.nandi@gmail.com>
2022-07-27 22:43:10 -0700

    barely done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
 console.log('%c HI', 'color: firebrick')
+
+function getImage() {
+    fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(response => response.json())
+    .then(data => {
+        data.message.forEach((images) => addImage(images))
+    })
+};
+
+function addImage(dogPic) {
+    let container = document.querySelector('#dog-image-container');
+    let image2 = document.createElement('img');
+    image2.src = dogPic;
+    container.append(image2);
+}
+
+function getBreed() {
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then(response => response.json())
+    .then(data => {
+        const breeds = Object.keys(data.message);
+        //const ul = document.querySelector('#dog-breeds');
+        updateBreeds(breeds);
+        dropDown();
+    })
+}
+
+function updateBreeds(breeds) {
+    breeds.forEach(breed => addBreed(breed));
+}
+
+function addBreed(breed) {
+    const ul = document.querySelector('#dog-breeds');
+    //console.log(ul);
+    const li = document.createElement('li');
+    li.innerText = breed;
+    ul.append(li);
+    li.addEventListener('click', updateColor);
+}
+
+function updateColor(e) {
+    e.target.style.color = "palevioletred";
+}
+function selectBreeds(letter) {
+    updateBreeds(breeds.filter(breed => breed.startsWith(letter)));
+}
+
+function dropDown() {
+    let breedDrop = document.querySelector('#breed-dropdown');
+    breedDrop.addEventListener('change', function(event) {
+        selectBreeds(event.target.value);
+    });
+}
+
+
+function dogs() {
+    getImage();
+    getBreed();
+}
+
+document.addEventListener("DOMContentLoaded", dogs());
